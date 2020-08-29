5b1749a9ff82567e3340ee5e50952d272ec94c92
sujeetkrv <surya7436gupta@gmail.com>
2020-08-29 13:43:26 +0530

    first commit to azure

---
+++ src/com/cg/loan/client/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.cg.loan.client;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Sujeet...!");
+        System.out.println("Hello World...!");
+        System.out.println("Hello Github!....................");
+    }
+}
